3a5f79d154bf411385214848ef598072be4862ad
fanov <fanovalesya@gmail.com>
2026-05-07 11:00:17 +0700

    h

--- Phonebook_lab9/App.xaml.cs
+++ Phonebook_lab9/App.xaml.cs
@@ -15,7 +15,32 @@ namespace Phonebook_lab9
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            
+            // 1. Создаём коллекцию сервисов
+            var services = new ServiceCollection();
+            // 2. Регистрируем сервисы (Lifetime)
+            // DialogService — Singleton, так как он не хранит
+            // состояние пользователя.
+            services.AddSingleton<IDialogService, DialogService>();
+            // 3. ViewModel — Transient (при навигации нам будут
+            // нужны новые экземпляры)
+            services.AddTransient<MainViewModel>();
+            // 4. Главное окно — Singleton с явной передачей
+            // DataContext через лямбда-выражение
+            services.AddSingleton<MainWindow>(sp =>
+            {
+                var window = new MainWindow();
+                window.DataContext =
+                sp.GetRequiredService<MainViewModel>();
+                return window;
+            });
+            // 5. Создаём контейнер (ServiceProvider)
+            var serviceProvider =
+            services.BuildServiceProvider();
+            // 6. Получаем главное окно и запускаем его
+            var mainWindow =
+            serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
         }
     }
 
